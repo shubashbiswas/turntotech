1e5a6709dcdf68390036e3acdc2e55f3e581fc08
Shubash Kumar Biswas <79921627+shubashbiswas@users.noreply.github.com>
2026-06-14 11:15:17 +0600

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,52 @@
+import "./globals.css";
+
+import { Inter as FontSans } from "next/font/google";
+import { ThemeProvider } from "@/components/theme/theme-provider";
+import { Nav } from "@/components/layout/nav";
+import { Footer } from "@/components/layout/footer";
+import { Analytics } from "@vercel/analytics/react";
+
+import { siteConfig } from "@/site.config";
+import { cn } from "@/lib/utils";
+
+import type { Metadata } from "next";
+
+const font = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "WordPress & Next.js Starter by 9d8",
+  description:
+    "A starter template for Next.js with WordPress as a headless CMS.",
+  metadataBase: new URL(siteConfig.site_domain),
+  alternates: {
+    canonical: "/",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body className={cn("min-h-screen font-sans antialiased", font.variable)}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Nav />
+          {children}
+          <Footer />
+        </ThemeProvider>
+        <Analytics />
+      </body>
+    </html>
+  );
+}
